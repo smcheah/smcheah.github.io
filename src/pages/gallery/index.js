fba3f68f2f9dc181e54b70e41add64e2045fa8f3
Simone Cheah <simone_cheah@hotmail.com>
2021-02-16 10:55:25 +1100

    add detail links

--- src/pages/gallery/index.js
+++ src/pages/gallery/index.js
@@ -1,168 +1,174 @@
+import { useState } from "react"
 import { BrowserRouter as Router, Route, Switch, Link, useRouteMatch, useParams } from "react-router-dom";
-import Detail from "../website-detail";
+import Footer from "../../components/Footer";
+import Detail from "../../components/Detail";
+import burgerPic from '../../images/burger-restaurant.png';
+import mealPic from '../../images/meal-budgeter.gif';
+import teamsummaryPic from '../../images/team-summary.gif';
+import bandearPic from '../../images/BandEaR.png';
+import codequizPic from '../../images/code-quiz.gif';
+import workplannerPic from '../../images/work-planner.gif';
+import weatherdashPic from '../../images/weather-dashboard.png';
+import birdfactsPic from '../../images/bird-facts.gif';
 
 const GalleryPage = () => {
     let { path, url } = useRouteMatch();
+    const [modal, setModal] = useState(false)
+    
+    const showModal = (e) => {
+        e.preventDefault();
+        setModal(true);
+    };
+    const closeModal = (e) => {
+        e.preventDefault();
+        setModal(false);
+    };
 
-    return <div>
+    return <Router>
         <nav id="page-top">
-            <div class="nav-items">
-                <Link to="/" class="fas fa-igloo home"></Link>
+            <div className="nav-items">
+                <Link to="/" className="fas fa-igloo home"></Link>
                 {/* <!-- <a href="#design-sites">Design</a>
             <a href="#technical-sites">Technical</a> --> */}
-                <button class="fas fa-bars ham"></button>
+                <button className="fas fa-bars ham"></button>
             </div>
         </nav>
 
-        <ul>
-            <li>
-                <Link to={ `${url}/website1` }>website 1 details</Link>
-            </li>
-            <li>
-                <Link to={ `${url}/website2` }>website 2 details</Link>
-            </li>
-            <li>
-                <Link to={ `${url}/website3` }>website 3 details</Link>
-            </li>
-        </ul>
-
         <Switch>
-            <Route exact path={ path }>
-                <h3>Please select a topic.</h3>
-            </Route>
             <Route exact path={ `${path}/:detail` }>
-                <Detail />
+                <Detail
+                    showModal={ showModal }
+                    closeModal={ closeModal } />
             </Route>
         </Switch>
 
-        <main class="gallery-list">
-            <div class="gallery-item">
-                <img src="images/burger-restaurant.png" alt="burger restaurant interface" />
+        <main className="gallery-list">
+            <div className="gallery-item">
+                <img src={ burgerPic } alt="burger restaurant interface" />
 
-                <div class="content">
-                    <div class="heading">
+                <div className="content">
+                    <div className="heading">
                         <h2>Burger Restaurant</h2>
-                        <a href="https://github.com/smcheah/burger-restaurant" class="fab fa-github-square" target="_blank"></a>
+                        <a href="https://github.com/smcheah/burger-restaurant" className="fab fa-github-square" target="_blank"></a>
                     </div>
 
                     <p>Last Updated: 08/11/2020</p>
-                    <a href="product-description">Overview</a>
+                    <Link to={ `${url}/burger-restaurant` }>Development Overview</Link>
                     <a href="https://thawing-cliffs-82345.herokuapp.com/" target="_blank">Site link</a>
                 </div>
             </div>
 
-            <div class="gallery-item">
-                <img src="images/meal-budgeter.gif" alt="meal budgeter interface" />
+            <div className="gallery-item">
+                <img src={ mealPic } alt="meal budgeter interface" />
 
-                <div class="content">
-                    <div class="heading">
+                <div className="content">
+                    <div className="heading">
                         <h2>Meal Budgeter</h2>
-                        <a href="https://github.com/smcheah/Meal-Budgeter-Project-2" class="fab fa-github-square" target="_blank"></a>
+                        <a href="https://github.com/smcheah/Meal-Budgeter-Project-2" className="fab fa-github-square" target="_blank"></a>
                     </div>
 
                     <p>Last Updated: 22/11/2020</p>
-                    <a href="product-description">Overview</a>
+                    <Link to={ `${url}/meal-budgeter` }>Development Overview</Link>
                     <a href="https://vast-crag-85624.herokuapp.com/" target="_blank">Site link</a>
                 </div>
             </div>
 
-            <div class="gallery-item">
-                <img src="images/team-summary.gif" alt="team summary interface" />
+            <div className="gallery-item">
+                <img src={ teamsummaryPic } alt="team summary interface" />
 
-                <div class="content">
-                    <div class="heading">
+                <div className="content">
+                    <div className="heading">
                         <h2>Team Summary</h2>
-                        <a href="https://github.com/smcheah/team-summary-template-engine" class="fab fa-github-square" target="_blank"></a>
+                        <a href="https://github.com/smcheah/team-summary-template-engine" className="fab fa-github-square" target="_blank"></a>
                     </div>
 
                     <p>Last Updated: 11/10/2020</p>
-                    <a href="product-description">Overview</a>
+                    <Link to={ `${url}/team-summary` }>Development Overview</Link>
                     <p>This application works in the terminal</p>
                 </div>
             </div>
 
-            <div class="gallery-item">
-                <img src="images/BandEaR.png" alt="band ear interface" />
+            <div className="gallery-item">
+                <img src={ bandearPic } alt="band ear interface" />
 
-                <div class="content">
-                    <div class="heading">
+                <div className="content">
+                    <div className="heading">
                         <h2>BandEaR</h2>
-                        <a href="https://github.com/smcheah/BandEaR-Project-1" class="fab fa-github-square" target="_blank"></a>
+                        <a href="https://github.com/smcheah/BandEaR-Project-1" className="fab fa-github-square" target="_blank"></a>
                     </div>
 
                     <p>Last Updated: 20/09/2020</p>
-                    <a href="product-description">Overview</a>
+                    <Link to={ `${url}/BandEaR` }>Development Overview</Link>
                     <a href="https://chrisjmckeown.github.io/BandEaR_Discover_live_artists/" target="_blank">Site link</a>
                 </div>
             </div>
 
-            <div class="gallery-item">
-                <img src="images/code-quiz.gif" alt="code quiz interface" />
+            <div className="gallery-item">
+                <img src={ codequizPic } alt="code quiz interface" />
 
-                <div class="content">
-                    <div class="heading">
+                <div className="content">
+                    <div className="heading">
                         <h2>Code Quiz</h2>
-                        <a href="https://github.com/smcheah/Code-Quiz" class="fab fa-github-square" target="_blank"></a>
+                        <a href="https://github.com/smcheah/Code-Quiz" className="fab fa-github-square" target="_blank"></a>
                     </div>
 
                     <p>Last Updated: 27/09/2020</p>
-                    <a href="product-description">Overview</a>
+                    <Link to={ `${url}/code-quiz` }>Development Overview</Link>
                     <a href="https://smcheah.github.io/Code-Quiz/" target="_blank">Site link</a>
                 </div>
             </div>
 
-            <div class="gallery-item">
-                <img src="images/work-planner.gif" alt="work planner interface" />
+            <div className="gallery-item">
+                <img src={ workplannerPic } alt="work planner interface" />
 
-                <div class="content">
-                    <div class="heading">
+                <div className="content">
+                    <div className="heading">
                         <h2>Work Planner</h2>
-                        <a href="https://github.com/smcheah/Work-Planner" class="fab fa-github-square" target="_blank"></a>
+                        <a href="https://github.com/smcheah/Work-Planner" className="fab fa-github-square" target="_blank"></a>
                     </div>
 
                     <p>Last Updated: 06/09/2020</p>
-                    <a href="product-description">Overview</a>
+                    <Link to={ `${url}/work-planner` }>Development Overview</Link>
                     <a href="https://smcheah.github.io/Work-Planner/" target="_blank">Site link</a>
                 </div>
             </div>
 
-            <div class="gallery-item">
-                <img src="images/weather-dashboard.png" alt="Weather dashboard" />
+            <div className="gallery-item">
+                <img src={ weatherdashPic } alt="Weather dashboard" />
 
-                <div class="content">
-                    <div class="heading">
+                <div className="content">
+                    <div className="heading">
                         <h2>Weather Dashboard</h2>
-                        <a href="https://github.com/smcheah/Weather-Dashboard" class="fab fa-github-square" target="_blank"></a>
+                        <a href="https://github.com/smcheah/Weather-Dashboard" className="fab fa-github-square" target="_blank"></a>
                     </div>
 
                     <p>Last Updated: 13/09/2020</p>
-                    <a href="product-description">Overview</a>
+                    <Link to={ `${url}/weather-dashboard` }>Development Overview</Link>
                     <a href="https://smcheah.github.io/Weather-Dashboard/" target="_blank">Site link</a>
                 </div>
             </div>
 
-            <div class="gallery-item">
-                <img src="images/bird-facts.gif" alt="Bird facts website" />
+            <div className="gallery-item">
+                <img src={ birdfactsPic } alt="Bird facts website" />
 
-                <div class="content">
-                    <div class="heading">
+                <div className="content">
+                    <div className="heading">
                         <h2>Bird Facts</h2>
-                        <a href="https://github.com/smcheah/project0" class="fab fa-github-square" target="_blank"></a>
+                        <a href="https://github.com/smcheah/project0" className="fab fa-github-square" target="_blank"></a>
                     </div>
 
                     <p>Last Updated: 03/11/2019</p>
-                    <a href="product-description">Overview</a>
+                    <Link to={ `${url}/bird-facts` }>Development Overview</Link>
                     <a href="https://smcheah.github.io/project0/birds.html" target="_blank">site link</a>
                 </div>
             </div>
 
         </main>
 
-        <div class="decor"></div>
-        <footer>
-            <div>Simone Cheah &copy; Copyright 2020</div>
-        </footer>
-    </div>;
+
+
+        <Footer />
+    </Router>;
 
 };
 
