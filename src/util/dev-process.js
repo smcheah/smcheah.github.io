40eb95bea4cba15c29a506e84ead68282db68dcf
Simone Cheah <simone_cheah@hotmail.com>
2021-02-16 10:56:13 +1100

    add detail data

---
+++ src/util/dev-process.js
@@ -0,0 +1,42 @@
+const DevDetails = {
+    // "url-detail": {
+    //     title: '',
+    //     lastUpdated: '',
+    //     description: '',
+    //     planning: '',
+    //     wireframeImgs: [],
+    //     prototypeImgs: '',
+    //     issues: [],
+    //     siteImgs: [],
+    //     siteLink: ''
+    // },
+    "burger-restaurant": {
+        title: 'Burger Restaurant',
+        lastUpdated: '08/11/2020',
+        description: 'hello',
+        planning: 'hello',
+        wireframeImgs: [
+            'https://picsum.photos/100',
+            'https://picsum.photos/100',
+        ],
+        prototypeImgs: ['https://picsum.photos/100'],
+        issues: [
+            'hello'
+        ],
+        siteImgs: ['https://picsum.photos/100'],
+        siteLink: 'hello'
+    },
+    "meal-budgeter": {
+        title: 'Meal Budgeter',
+        lastUpdated: '',
+        description: '',
+        planning: '',
+        wireframeImgs: [],
+        prototypeImgs: '',
+        issues: [],
+        siteImgs: [],
+        siteLink: ''
+    },
+};
+
+export default DevDetails;
