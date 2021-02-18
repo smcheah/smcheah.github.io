1013d6fa3551f7b13f9fe8fd9333bf509e646851
Simone Cheah <simone_cheah@hotmail.com>
2021-02-18 20:49:15 +1100

    added more info to dev details

--- src/util/dev-process.js
+++ src/util/dev-process.js
@@ -5,7 +5,7 @@ const DevDetails = {
     //     description: '',
     //     planning: '',
     //     wireframeImgs: [],
-    //     prototypeImgs: '',
+    //     prototypeImgs: [],
     //     issues: [],
     //     siteImgs: [],
     //     siteLink: ''
@@ -13,30 +13,111 @@ const DevDetails = {
     "burger-restaurant": {
         title: 'Burger Restaurant',
         lastUpdated: '08/11/2020',
-        description: 'hello',
-        planning: 'hello',
+        description: 'Eat Da Burger! is a restaurant app that lets users input the names of burgers they would like to eat. Submit a burger name to display the burger on the left side of the page -- waiting to be devoured.',
+        planning: '',
         wireframeImgs: [
-            'https://picsum.photos/100',
-            'https://picsum.photos/100',
+            'https://i.imgur.com/mxX1jV5.png'
         ],
-        prototypeImgs: ['https://picsum.photos/100'],
+        prototypeImgs: [
+            'https://i.imgur.com/0OhcTbP.png'
+        ],
         issues: [
-            'hello'
+            'could have more detailed side menus',
+            'site not accurate to wireframe',
         ],
-        siteImgs: ['https://picsum.photos/100'],
-        siteLink: 'hello'
+        siteImgs: [],
+        siteLink: 'https://thawing-cliffs-82345.herokuapp.com/'
     },
     "meal-budgeter": {
         title: 'Meal Budgeter',
-        lastUpdated: '',
-        description: '',
+        lastUpdated: '22/11/2020',
+        description: 'Our application allows users to search for specific recipes and be presented with details about said recipe including ingredients, method, and an image of the finished meal.',
+        planning: 'As most people live on a budget we thought that this application could serve to aid in people’s day to day lives especially during the COVID pandemic in which funds are scarce. In particular this was aimed at University students who struggle to come up with cost-effective, and creating meals when living out of home.',
+        wireframeImgs: [
+            'https://i.imgur.com/Iw6SRkd.png',
+            'https://i.imgur.com/vqO1OkJ.png'
+        ],
+        prototypeImgs: ['https://media.giphy.com/media/2Hz1zi30Q0Q4dFef7t/giphy.gif', 'https://media.giphy.com/media/EVbDIZoo0nksWwPaoW/giphy.gif'],
+        issues: [
+            'overall website styling',
+            'should be responsive',
+            'recipe editor is not user friendly',
+            'images are not compressed'
+        ],
+        siteImgs: [],
+        siteLink: 'https://vast-crag-85624.herokuapp.com/'
+    },
+    "team-summary": {
+        title: 'Team Summary',
+        lastUpdated: '11/10/2020',
+        description: 'A Node CLI that takes in information about employees and generates a HTML webpage that displays a team summary for each person.',
         planning: '',
         wireframeImgs: [],
-        prototypeImgs: '',
+        prototypeImgs: [],
         issues: [],
         siteImgs: [],
         siteLink: ''
     },
+    "BandEaR": {
+        title: 'BandEar',
+        lastUpdated: '20/09/2020',
+        description: 'Web application that allows users to search various artists, preview their music and see all upcoming events.',
+        planning: '',
+        wireframeImgs: [
+            'https://i.imgur.com/wdQ1RW8.png'
+        ],
+        prototypeImgs: [],
+        issues: [],
+        siteImgs: ['https://raw.githubusercontent.com/smcheah/BandEaR-Project-1/master/Assets/Images/Mockup-190920.png'],
+        siteLink: 'https://chrisjmckeown.github.io/BandEaR_Discover_live_artists/'
+    },
+    "code-quiz": {
+        title: 'Code Quiz',
+        lastUpdated: '',
+        description: '',
+        planning: '',
+        wireframeImgs: [],
+        prototypeImgs: [],
+        issues: [],
+        siteImgs: [],
+        siteLink: ''
+    },
+    "work-planner": {
+        title: 'Work Planner',
+        lastUpdated: '',
+        description: '',
+        planning: '',
+        wireframeImgs: [],
+        prototypeImgs: [],
+        issues: [],
+        siteImgs: [],
+        siteLink: ''
+    },
+    "weather-dashboard": {
+        title: 'Weather Dashboard',
+        lastUpdated: '',
+        description: '',
+        planning: '',
+        wireframeImgs: [
+            'https://imgur.com/6eIuz05'
+        ],
+        prototypeImgs: [],
+        issues: [],
+        siteImgs: [],
+        siteLink: ''
+    },
+    "bird-facts": {
+        title: '',
+        lastUpdated: '',
+        description: '',
+        planning: '',
+        wireframeImgs: [],
+        prototypeImgs: [],
+        issues: [],
+        siteImgs: [],
+        siteLink: ''
+    },
+
 };
 
 export default DevDetails;
